a8e66d0a455bd07e2548ab2213e56b849dfa90c6
Harrison Reich <hreich1998@gmail.com>
2022-01-11 12:27:24 -0800

    code

--- seeders/postSeed.js
+++ seeders/postSeed.js
@@ -2,3 +2,12 @@ require('dotenv').config()
 
 const { User, Post } = require('../models')
 const sequelize = require('../db')
+
+async function seeder() {
+  await sequelize.sync({ force: true })
+  console.log('-----Seeding Data-----')
+
+  try {
+    await
+  }
+}
