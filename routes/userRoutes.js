c08fd7e7b5031b9c977adacdb5b954e567ab9662
Harrison Reich <hreich1998@gmail.com>
2022-01-11 12:37:11 -0800

    code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,3 +2,10 @@ const router = require('express').Router()
 const { User, Post } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
+
+router.post('/users/register', (req, res) => {
+  User.register(new User({ username: req.body.username, email: req.body.email }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
