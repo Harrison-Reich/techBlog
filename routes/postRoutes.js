4de18a17882f26f11b3eef78227eff4520e8dc06
Harrison Reich <hreich1998@gmail.com>
2022-01-11 12:34:10 -0800

    get posts function

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router()
 const { Post, User, Note } = require('../models')
 const passport = require('passport')
+
+// GET all posts
+router.get('/posts', passport.authenticate('jwt'), async function (req, res) {
+  const posts = await Post.findAll({ include: [User, Note] })
+  res.json(posts)
+})
+
+router.get('/posts/:id', passport.authenticate('jwt'), async function (req, res) {
+  const posts = await Post.findOne({ where: { id: req.params.id }, include: [User, Note] })
+  res.json(posts)
+})
