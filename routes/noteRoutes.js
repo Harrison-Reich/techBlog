b43e45378594f89eb3d1e8bf49bd04011f4335a2
Harrison Reich <hreich1998@gmail.com>
2022-01-11 12:32:18 -0800

    get comments function

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
 const { Post, User, Note } = require('../models')
 const passport = require('passport')
+
+// GET all comments
+router.get('/notes', passport.authenticate('jwt'), async function (req, res) {
+  const notes = await Note.findAll({ include: [User, Post] })
+  res.json(notes)
+})
