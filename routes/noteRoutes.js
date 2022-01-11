79bd23557d230fa37c3f883c5529dccf210d3714
Harrison Reich <hreich1998@gmail.com>
2022-01-11 12:32:31 -0800

    post a comment function

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -7,3 +7,13 @@ router.get('/notes', passport.authenticate('jwt'), async function (req, res) {
   const notes = await Note.findAll({ include: [User, Post] })
   res.json(notes)
 })
+
+// POST one comment
+router.post('/notes', passport.authenticate('jwt'), async function (req, res) {
+  const note = await Note.create({
+    body: req.body.body,
+    pid: req.body.pid,
+    uid: req.user.id
+  })
+  res.json(note)
+})
