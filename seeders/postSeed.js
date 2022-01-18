a382db587fd5f9f5777d7a99764168656458e620
Harrison Reich <hreich1998@gmail.com>
2022-01-18 04:28:16 -0800

    code

--- seeders/postSeed.js
+++ seeders/postSeed.js
@@ -6,17 +6,17 @@ const seed = [
   },
   {
     title: 'John hates Jack',
-    body: 'Nobody friend Jack on here he is lame lol',
+    body: 'Nobody friend Jack on here he must not know how to update his profile',
     uid: 1
   },
   {
     title: 'Jane is Best',
-    body: 'John thinks he is so cool but he is not really',
+    body: 'John thinks he is so cool but he is only kind of cool',
     uid: 2
   },
   {
     title: 'Jake',
-    body: 'Yes hi blog.com I am Jake',
+    body: 'Yes hi techblog.com I am Jake',
     uid: 3
   }
 ]
